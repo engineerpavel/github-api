566f8138ea16c48ce49bb6ee16f2d351fb1b9116
Pavel Pogorelov <engineer.pavel@yandex.com>
2021-10-15 17:55:09 +0300

    storage

--- src/app/app-common/services/github-search.service.ts
+++ src/app/app-common/services/github-search.service.ts
@@ -2,9 +2,21 @@ import {Injectable} from '@angular/core';
 import {GithubSearchRepository} from '../repositories/github-search.repository';
 import {Observable, ReplaySubject} from 'rxjs';
 import {GithubRepoInterface} from '../interfaces/github-repo.interface';
+import {shareReplay} from 'rxjs/operators';
 
 @Injectable()
 export class GithubSearchService {
+
+  get repos() {
+    return this._repos;
+  }
+
+  /**
+   * Storage for current search result
+   * @private
+   */
+  private _repos: ReplaySubject<Observable<GithubRepoInterface[]>> = new ReplaySubject<Observable<GithubRepoInterface[]>>(1);
+
   constructor(private readonly repo: GithubSearchRepository) {
   }
 
@@ -12,7 +24,7 @@ export class GithubSearchService {
    * Get info about repositories by organisation name
    * @param orgName organisation name
    */
-  public getOrganisationRepos(orgName: string): Observable<GithubRepoInterface[]> {
-    return this.repo.getOrganisationRepos(orgName);
+  public getOrganisationRepos(orgName: string): void {
+    this._repos.next(this.repo.getOrganisationRepos(orgName).pipe(shareReplay(1)));
   }
 }
